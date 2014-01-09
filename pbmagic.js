1ce0bd97c45470c2f8e0d9d1f8325edb77aced9b
Cassidy Williams <cassidoo@outlook.com>
2014-01-09 12:18:08 -0600

    Initial JavaScript commit

---
+++ pbmagic.js
@@ -0,0 +1,30 @@
+/*
+ * Progress Bar Magic
+ * A project by the Honourable Cassidy Williams.
+ * Actually she's just a regular person.
+ * But hey, she made it.
+ * Here's her website: cassidoo.co
+ * Her Github: github.com/cassidoo
+ *
+ * And make sure you have jQuery when you use this.
+ * */
+
+function pbmagic(id, type, progress, total)
+/* id = the id of the bar
+ *  type = type of progress bar
+ *  progress = progress so far
+ *  total = value of 100% of the bar
+ * */
+{
+    var bar = document.getElementById(id);
+    //var levelBar = $('.level');
+
+    // Valid type values: "", battery
+    switch(type)
+    {
+        case "battery":
+            break;
+        default:
+            // I'll make a default bar that's moderately attractive
+    }
+}
